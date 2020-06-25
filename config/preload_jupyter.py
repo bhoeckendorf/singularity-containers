3cc1a55115be556d048588ffd29368e0ceaa7971
Burkhard Hoeckendorf <burkhard.hoeckendorf@pm.me>
2020-06-25 14:33:19 -0500

    Add development environment container

---
+++ config/preload_jupyter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+ 
+import os
+import subprocess
+ 
+def dask_setup(service):
+    # Start Jupyter, adjust to your paths and environment.
+    # As a reminder, subprocess.Popen works like this ["/path/to/executable", "--arg", "value", "--keywordarg=keywordvalue"]
+    #os.environ["JUPYTER_ALLOW_INSECURE_WRITES"] = "true"
+    proc = subprocess.Popen(["/opt/jupyter/bin/jupyter-lab", "--config=/opt/jupyter/etc/jupyter/jupyter_notebook_config.py"])
+    service.jlab_proc = proc
+
+def dask_teardown(service):
+    try:
+        proc = service.jlab_proc
+        proc.terminate()
+    except Exception:
+        pass
