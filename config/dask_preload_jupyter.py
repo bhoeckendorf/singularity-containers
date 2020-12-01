b3692c7df5c9f4861f8155ffefa99c03c12408f6
Burkhard Hoeckendorf <burkhard.hoeckendorf@pm.me>
2020-12-01 12:40:47 -0600

    Improvements and additions

--- config/dask_preload_jupyter.py
+++ config/dask_preload_jupyter.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
- 
+
 import os
 import subprocess
- 
+
+
 def dask_setup(service):
     # Start Jupyter, adjust to your paths and environment.
     # As a reminder, subprocess.Popen works like this ["/path/to/executable", "--arg", "value", "--keywordarg=keywordvalue"]
     #os.environ["JUPYTER_ALLOW_INSECURE_WRITES"] = "true"
-    proc = subprocess.Popen(["/opt/conda/bin/jupyter lab", "--config=/opt/conda/etc/jupyter/jupyter_notebook_config.py"])
+    proc = subprocess.Popen(["/opt/conda/bin/jupyter lab", "--config=/opt/conda/etc/jupyter/jupyter_config.py"])
     service.jlab_proc = proc
+
 
 def dask_teardown(service):
     try:
